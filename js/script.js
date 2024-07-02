bd1680b68cb3849403457b3ff8a24db0bb768c8b
Kyrylko <kkuzmenko1992@gmail.com>
2024-07-01 22:57:36 -0230

    0

--- js/script.js
+++ js/script.js
@@ -19,43 +19,6 @@ var customIcon3 = L.icon({
   popupAnchor: [-3, -76],
 });
 
-// WebSocket соединение с сервером на Heroku
-const socket = new WebSocket("wss://server-olivky.herokuapp.com");
-
-socket.onopen = function () {
-  console.log("WebSocket connection established.");
-};
-
-socket.onerror = function (error) {
-  console.error("WebSocket error: ", error);
-};
-
-socket.onmessage = function (event) {
-  const data = JSON.parse(event.data);
-
-  if (data.type === "initial-markers") {
-    data.markers.forEach((marker) => {
-      addMarkerAndCircle(marker.lat, marker.lng, getIconFromType(marker.type));
-    });
-  } else if (data.type === "new-marker") {
-    addMarkerAndCircle(
-      data.marker.lat,
-      data.marker.lng,
-      getIconFromType(data.marker.type)
-    );
-  }
-};
-
-function addMarkerAndCircle(lat, lng, icon) {
-  var marker = L.marker([lat, lng], { icon: icon }).addTo(map);
-  var circle = L.circle([lat, lng], { radius: 100 }).addTo(map);
-
-  setTimeout(function () {
-    map.removeLayer(marker);
-    map.removeLayer(circle);
-  }, 5000);
-}
-
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -66,6 +29,27 @@ if (navigator.geolocation) {
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution: "© OpenStreetMap contributors",
       }).addTo(map);
+
+      var socket = new WebSocket("wss://server-olivky.herokuapp.com");
+
+      socket.onopen = function () {
+        console.log("Соединение установлено.");
+      };
+
+      socket.onmessage = function (event) {
+        const data = JSON.parse(event.data);
+        if (data.type === "initial-markers") {
+          data.markers.forEach((marker) => {
+            addMarkerAndCircle(marker.lat, marker.lng, marker.icon);
+          });
+        } else if (data.type === "new-marker") {
+          addMarkerAndCircle(
+            data.marker.lat,
+            data.marker.lng,
+            data.marker.icon
+          );
+        }
+      };
 
       map.on("click", function (e) {
         const lat = e.latlng.lat;
@@ -79,17 +63,7 @@ if (navigator.geolocation) {
         icon1Button.style.height = "38px";
         icon1Button.src = customIcon1.options.iconUrl;
         icon1Button.onclick = function () {
-          // Отправляем новую метку на сервер
-          const markerData = {
-            type: "new-marker",
-            marker: {
-              lat: lat,
-              lng: lng,
-              type: "icon1", // Можно задать тип метки для дальнейшей обработки на сервере
-            },
-          };
-          socket.send(JSON.stringify(markerData));
-          addMarkerAndCircle(lat, lng, customIcon1);
+          sendMarker(lat, lng, customIcon1);
           map.closePopup();
         };
         popupContent.appendChild(icon1Button);
@@ -100,16 +74,7 @@ if (navigator.geolocation) {
         icon2Button.style.height = "38px";
         icon2Button.src = customIcon2.options.iconUrl;
         icon2Button.onclick = function () {
-          const markerData = {
-            type: "new-marker",
-            marker: {
-              lat: lat,
-              lng: lng,
-              type: "icon2",
-            },
-          };
-          socket.send(JSON.stringify(markerData));
-          addMarkerAndCircle(lat, lng, customIcon2);
+          sendMarker(lat, lng, customIcon2);
           map.closePopup();
         };
         popupContent.appendChild(icon2Button);
@@ -120,22 +85,28 @@ if (navigator.geolocation) {
         icon3Button.style.height = "48px";
         icon3Button.src = customIcon3.options.iconUrl;
         icon3Button.onclick = function () {
-          const markerData = {
-            type: "new-marker",
-            marker: {
-              lat: lat,
-              lng: lng,
-              type: "icon3",
-            },
-          };
-          socket.send(JSON.stringify(markerData));
-          addMarkerAndCircle(lat, lng, customIcon3);
+          sendMarker(lat, lng, customIcon3);
           map.closePopup();
         };
         popupContent.appendChild(icon3Button);
 
         L.popup().setLatLng([lat, lng]).setContent(popupContent).openOn(map);
       });
+
+      function sendMarker(lat, lng, icon) {
+        const marker = { lat, lng, icon };
+        socket.send(JSON.stringify({ type: "new-marker", marker }));
+      }
+
+      function addMarkerAndCircle(lat, lng, icon) {
+        var marker = L.marker([lat, lng], { icon: icon }).addTo(map);
+        var circle = L.circle([lat, lng], { radius: 100 }).addTo(map);
+
+        setTimeout(function () {
+          map.removeLayer(marker);
+          map.removeLayer(circle);
+        }, 5000);
+      }
     },
     function (error) {
       console.error("Ошибка при определении местоположения: ", error);
